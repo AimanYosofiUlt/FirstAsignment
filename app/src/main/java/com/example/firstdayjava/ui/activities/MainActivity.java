1d68ce25867375a5837de1b0bab2fc2f805b0aba
Youssif <youssifhamdy83@gmail.com>
2022-03-21 10:24:09 +0200

    ..

--- app/src/main/java/com/example/firstdayjava/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/firstdayjava/ui/activities/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         Objects.requireNonNull(getSupportActionBar()).hide();
-/*
+
         Window window = this.getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
-*/
+
         if (getIntent().getStringExtra(LANGUAGE) != null) {
             changeLang(getIntent().getStringExtra(LANGUAGE));
         }
